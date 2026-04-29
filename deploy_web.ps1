150568b9e6cab36a0d1fdde1629992693c2d8241
Nils-Inpeek <171995489+Nils-Rindlisbacher@users.noreply.github.com>
2026-04-29 08:56:27 +0200

    fix: deployment script

--- deploy_web.ps1
+++ deploy_web.ps1
@@ -19,12 +19,21 @@ Write-Host "Bereinige alten gh-pages Branch..." -ForegroundColor Cyan
 git branch -D gh-pages -ErrorAction SilentlyContinue
 git push origin --delete gh-pages -ErrorAction SilentlyContinue
 
-# 4. Subtree erzeugen
-Write-Host "Erzeuge neuen gh-pages Branch via Subtree..." -ForegroundColor Cyan
-git subtree split --prefix Application/Skaterz/build/web -b gh-pages
+# 4. Neuen gh-pages Branch erstellen
+Write-Host "Erstelle neuen gh-pages Branch..." -ForegroundColor Cyan
+git checkout --orphan gh-pages
+git rm -rf .
 
-# 5. Deployment pushen
-Write-Host "Pushe neuen gh-pages Branch..." -ForegroundColor Cyan
+# 5. Web-Build in Root kopieren
+Write-Host "Kopiere Web-Build..." -ForegroundColor Cyan
+Copy-Item -Path "Application/Skaterz/build/web/*" -Destination "." -Recurse -Force
+
+# 6. CNAME hinzufügen
+"ww.dsdsa.ch" | Out-File -Encoding ascii -FilePath "CNAME"
+
+# 7. Commit + Push
+git add .
+git commit -m $commitMessage
 git push -f origin gh-pages
 
 Write-Host "-------------------------------------------" -ForegroundColor Green
